59e1bfb11db21c2ea4bbdd57bc2b7f616d9998cf
Darina-Shvydiuk <darinakabakova@gmail.com>
2022-09-15 20:41:21 +0300

    finish refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,45 +22,9 @@ export const App = () => {
   console.log(contacts);
   const [filter, setFilter] = useState('');
 
-  // state = {
-  //   contacts: [
-  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  //   ],
-  //   filter: '',
-  // };
-
   useEffect(() => {
-    // const dataFromLocalStorage = getLocalData();
-    // console.log(dataFromLocalStorage);
-    // if (!dataFromLocalStorage) {
-    //   console.log(dataFromLocalStorage);
-    //   setContacts([
-    //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    //   ]);
-    // }
     localStorage.setItem(CONTACTS, JSON.stringify(contacts));
   }, [contacts]);
-
-  // componentDidMount() {
-  //   const contacts = localStorage.getItem('contacts');
-  //   const parsedContacts = JSON.parse(contacts);
-  //   if (parsedContacts) {
-  //     this.setState({ contacts: parsedContacts });
-  //   }
-  // }
-
-  // componentDidUpdate(prevState, prevProps) {
-  //   const { contacts } = this.state;
-  //   if (contacts !== prevState.contacts) {
-  //     localStorage.setItem('contacts', JSON.stringify(contacts));
-  //   }
-  // }
 
   const handelFormSubmit = data => {
     const contactName = data.name.toLowerCase();
